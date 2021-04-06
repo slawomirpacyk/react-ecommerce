862b5dc30f7c3f88a4add2115cadabc402cb6999
Sławomir Pacyk <slawomir.pacyk@gmail.com>
2021-04-06 15:08:10 +0200

    header component for shop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/header/header';
+
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 
@@ -9,6 +11,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
