74691776f409e5b95f6a35f88480eeda39f9a078
Sławomir Pacyk <slawomir.pacyk@gmail.com>
2021-05-06 16:48:38 +0200

    changes to collection page

---
+++ src/redux/shop/shop-selectors.js
@@ -0,0 +1,26 @@
+import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
+
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector(
+    [selectShop],
+    shop => shop.collections
+) 
+
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  )
+); 
+
+
+/* 
+export const selectCollection = memoize(collectionUrlParam => {
+    return createSelector(
+        [selectCollections],
+        collections => collections[collectionUrlParam]
+    )
+})
+*/
